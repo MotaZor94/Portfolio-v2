1db2a19738ad67ce9959ce3afd3e9cd710df9bf6
Marko Colic <markocolic@Markos-MacBook-Pro.local>
2019-01-26 23:49:05 +0100

    new updates,including new animation triggers,and new animations,also the contact button is included

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,15 @@
-// load modules
 const gulp = require('gulp');
 const del = require('del');
-const livereload = require('gulp-livereload');
 const htmlmin = require('gulp-htmlmin');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
-const csso = require('gulp-csso');
+// const csso = require('gulp-csso');
 const sourcemaps = require('gulp-sourcemaps');
 const fs   = require('fs');
-const notify = require('gulp-notify');
- 
+const terser = require('gulp-terser');
+
 // creates default src folder structure
-function createSrcStructure(done) {
+const createSrcStructure = (done) => {
     const folders = [
         'dist',
         'src',
@@ -19,88 +17,80 @@ function createSrcStructure(done) {
         'src/assets/fonts',
         'src/assets/img',
         'src/scss',
+        'src/js',
     ];
     const files = [
         'src/assets/index.html',
     ];
- 
+
     folders.forEach(dir => {
         if(!fs.existsSync(dir)) {
             fs.mkdirSync(dir);
             console.log('folder created:', dir);    
         }   
     });
- 
+
     files.forEach(file => {
         if(!fs.existsSync(file)) {
             fs.writeFileSync(file, '');
             console.log('file created:', file);    
         }   
     });
- 
+
     return done();
 }
- 
+
 // delete all the assets in dist
-function assetsClean(done) {
+const assetsClean = (done) => {
     return del(
-        ['dist/**/*', '!dist/**/*.css', '!dist/**/*.html'], 
+        ['dist/**/*', '!dist/**/*.css', '!dist/**/*.html', '!dist/**/*.js'], 
         { force: true }
     );
 }
  
 // Copy all assets from src/assets into dist
-function assetsPublish(done) {
+const assetsPublish = (done) => {
     return gulp.src('src/assets/**/*')
       .pipe(gulp.dest('dist'));
 }
- 
+
 // publish HTML files
-function htmlPublish(done) {
+const htmlPublish = (done) => {
     return gulp.src('src/assets/**/*.html')
       .pipe(htmlmin({ collapseWhitespace: true }))
-      .pipe(gulp.dest('dist'))
-      .pipe(livereload());
+      .pipe(gulp.dest('dist'));
 }
- 
+
 // compile SCSS files
-function scssCompile(done) {
+const scssCompile = (done) => {
     return gulp.src('src/scss/**/*.scss')
       .pipe(sourcemaps.init())
-      .pipe(sass().on('error', function(err) {
-          notify.onError({
-              title: 'SCSS Compile',
-              message: 'Error',
-              sound: 'Beep'
-          })(err);
-          return done();
-      }))
+      .pipe(sass().on('error', sass.logError))
       .pipe(autoprefixer({
         browsers: ['last 2 versions'],
         cascade: false
       }))
     //   .pipe(csso())
       .pipe(sourcemaps.write('.'))
-      .pipe(gulp.dest('dist/css'))
-      .pipe(notify({
-        title: 'SCSS Compile',
-        message: 'Done'
-      }))
-      .pipe(livereload());
+      .pipe(gulp.dest('dist/css'));
+}
+
+// Gulp task to minify JavaScript files
+const minifyJs = (done) => {
+    return gulp.src('src/js/**/*.js')
+      .pipe(terser())
+      .pipe(gulp.dest('dist/js'));
 }
  
 // watch files
-function watchFiles(done) {
+const watchFiles = (done) => {
     gulp.watch("src/assets/**/*.html", htmlPublish);
     gulp.watch("src/scss/**/*.scss", scssCompile);
+    gulp.watch("src/JS/**/*.js", minifyJs);
 }
- 
-// start livereload
-function livereloadStart(done) {
-    livereload.listen();
-}
- 
+
+
 // export tasks
 exports.structure = createSrcStructure;
-exports.publish = gulp.series(assetsClean, assetsPublish);
-exports.watch = gulp.parallel(livereloadStart, watchFiles);
+exports.publish = gulp.series(assetsClean, assetsPublish, scssCompile, minifyJs);
+exports.watch = gulp.parallel(watchFiles);
---
+++ src/assets/JS/script.js
@@ -0,0 +1,11 @@
+let contact = document.querySelector("#contact");
+let container = document.querySelector("#takeMe");
+
+
+contact.addEventListener('click', () => {
+    container.classList.toggle("contShow");
+})
+
+
+
+
